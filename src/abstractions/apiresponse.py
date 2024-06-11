3c7ad0c2a455f7da8aa25d8e9e2af4842d6cfb95
Kalle Sundell <sundellkalle@gmail.com>
2024-06-11 10:47:18 +0300

    improve documentation

--- src/abstractions/apiresponse.py
+++ src/abstractions/apiresponse.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
+"""
+A uniform structure that represents a single headline received from the API.
+"""
 class APIResponse:
     def __init__(self, id: int, source: str, title: str, time: datetime):
         self.id = id
@@ -8,7 +11,11 @@ class APIResponse:
         self.time = time
 
     def __str__(self) -> str:
+        """(<time>) <AGENCY NAME>: <headline>"""
         return f"({str(self.time.hour).zfill(2)}:{str(self.time.minute).zfill(2)}) {(self.source.upper())}: {self.title}"
     
     def __eq__(self, other):
+        """Use ID for checking equality between APIResponse objects.
+        Headlines may change, but the id attribute should remain the same.
+        Most robust way to avoid duplicates."""
         return self.id == other.id
--- src/abstractions/feedhandler.py
+++ src/abstractions/feedhandler.py
@@ -5,6 +5,13 @@ import logging
 class FeedHandler:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
-    def get_articles() -> List[APIResponse]: pass
-    def fetch() -> List[dict]: pass
-    def parse() -> APIResponse: pass
+    def get_articles() -> List[APIResponse]:
+        """The main driver function that handles all the exception handling.
+        Calls fetch() and parse()."""
+        pass
+    def fetch() -> List[dict]:
+        """Makes a request to the news agency's API endpoint and returns the response as a list of dicts."""
+        pass
+    def parse() -> APIResponse: 
+        """Parses the individual REST entry and converts it into an APIResponse object."""
+        pass
