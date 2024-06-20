b8c5a390fd06d76175efe0ab078d608718639c30
Kalle Sundell <sundellkalle@gmail.com>
2024-06-20 11:36:43 +0300

    lint

--- src/abstractions/resthandler.py
+++ src/abstractions/resthandler.py
@@ -5,13 +5,16 @@ import logging
 class RestHandler:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
+
     def get_articles() -> List[APIResponse]:
         """The main driver function that handles all the exception handling.
         Calls fetch() and parse()."""
         pass
+
     def fetch() -> List[dict]:
         """Makes a request to the news agency's API endpoint and returns the response as a list of dicts."""
         pass
+
     def parse() -> APIResponse: 
         """Parses the individual REST entry and converts it into an APIResponse object."""
         pass
